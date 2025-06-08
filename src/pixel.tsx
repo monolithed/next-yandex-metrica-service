e1949b572f748d822f343b7357abc24488cea371
A.A <monolithed@gmail.com>
2025-06-09 01:27:52 +0700

    Removed unused code

--- src/pixel.tsx
+++ src/pixel.tsx
@@ -1,5 +1,4 @@
 import type React from 'react';
-
 import {Endpoints} from './endpoints';
 
 type Props = {
--- src/ym.ts
+++ src/ym.ts
@@ -1,38 +1,6 @@
-import type {
-    YandexMetricaEventParameters,
-    InitEventParameters,
-    AddFileExtensionEventParameters,
-    ExtLinkEventParameters,
-    FileEventParameters,
-    FirstPartyParamsEventParameters,
-    GetClientIDEventParameters,
-    HitEventParameters,
-    NotBounceEventParameters,
-    ParamsEventParameters,
-    ReachGoalEventParameters,
-    SetUserIDEventParameters,
-    UserParamsEventParameters
-} from '@/events';
+import type {YandexMetricaEventParameters} from '@/events';
 
-type Foo = [tagID: number | null, parameters: YandexMetricaEventParameters];
-
-// function ym<T extends Foo>(...foo: Foo[]): void;
-
-// function ym(tagID: null): void;
-// function ym(tagID: number, ...params: InitEventParameters): void;
-// function ym(tagID: number, ...params: AddFileExtensionEventParameters): void;
-// function ym(tagID: number, ...params: ExtLinkEventParameters): void;
-// function ym(tagID: number, ...params: FileEventParameters): void;
-// function ym(tagID: number, ...params: FirstPartyParamsEventParameters): void;
-// function ym(tagID: number, ...params: GetClientIDEventParameters): void;
-// function ym(tagID: number, ...params: HitEventParameters): void;
-// function ym(tagID: number, ...params: NotBounceEventParameters): void;
-// function ym(tagID: number, ...params: ParamsEventParameters): void;
-// function ym(tagID: number, ...params: ReachGoalEventParameters): void;
-// function ym(tagID: number, ...params: SetUserIDEventParameters): void;
-// function ym(tagID: number, ...params: UserParamsEventParameters): void;
-
-function ym<T extends YandexMetricaEventParameters>(tagID: number, ...params: T): void {
+const ym = <T extends YandexMetricaEventParameters>(tagID: number, ...params: T): void => {
     if (!tagID) {
         return void 0;
     }
